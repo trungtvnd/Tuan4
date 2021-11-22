7204e0eb20889f7dd88474add13b0c4010d5c05b
trungtvnd <tranviettrung20011992@gmail.com>
2021-11-22 13:44:48 +0700

    add file bai tap temperature

--- filebaitap/16-temperaature.js
+++ filebaitap/16-temperaature.js
@@ -1,16 +1,43 @@
-let Temperature = function (celcius){
-    this.celcius = celcius;
-    this.CtoK = function (){
-        return this.celcius + 273.15
+// let Temperature = function (celcius){
+//     this.celcius = celcius;
+//     this.CtoK = function (){
+//         return this.celcius + 273.15
+//     }
+//     this.CtoF = function (){
+//         return this.celcius * 1.8 + 32
+//     }
+//
+// }
+// let temperature = new Temperature()
+// temperature.celcius = 25
+// let k = temperature.CtoK()
+// let j = temperature.CtoF()
+// alert(k)
+// alert(j)
+
+class Temperature {
+    celcius
+
+    constructor(celcius) {
+        this.celcius = celcius
     }
-    this.CtoF = function (){
-        return this.celcius * 1.8 + 32
+    getC(){
+        return this.celcius
+    }
+    setC(nameSet){
+        return this.celcius = nameSet
     }
 
+    Ctok() {
+        return this.celcius + 273.15
+    }
+
+    CtoF() {
+        return this.celcius * 1.8 + 32
+    }
 }
-let temperature = new Temperature()
-temperature.celcius = 25
-let k = temperature.CtoK()
-let j = temperature.CtoF()
-alert(k)
-alert(j)
+let temperarure = new Temperature(25)
+temperarure.setC(36)
+console.log(temperarure.getC())
+console.log(temperarure.CtoF())
+console.log(temperarure.Ctok())
